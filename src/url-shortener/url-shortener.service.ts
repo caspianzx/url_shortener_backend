a2f6f16d7ca6f3a8eecef06a526d3cf411072946
caspianng <caspianzx@gmail.com>
2023-02-10 13:14:53 +0800

    add nanoid package to generate unique id

--- src/url-shortener/url-shortener.service.ts
+++ src/url-shortener/url-shortener.service.ts
@@ -3,20 +3,23 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Url, UrlDocument } from './url-shortener.schema';
 import { ShortenUrlDTO } from './dto/shorten-url.dto';
+import { nanoid } from 'nanoid';
 
 @Injectable()
 export class UrlShortenerService {
   constructor(@InjectModel(Url.name) private urlModel: Model<UrlDocument>) {}
 
   async shortenUrl(shortenUrlDto: ShortenUrlDTO): Promise<string> {
+
+    const shortened = nanoid(10)
     // test code. will change the logic later
-    const shortened = `abcde ${Math.floor(Math.random() * 10000)}`;
-    const doc = {
+    // const shortened = `abcde ${Math.floor(Math.random() * 10000)}`;
+    const url = {
       shortened,
       original: shortenUrlDto.originalUrl,
     };
 
-    const createdUrlDoc = new this.urlModel(doc);
+    const createdUrlDoc = new this.urlModel(url);
 
     await createdUrlDoc.save();
 
